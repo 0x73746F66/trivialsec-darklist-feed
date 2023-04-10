46e593f4f2724718180144bed42409cd7b11f2ce
chris <chris@trivialsec.com>
2023-04-10 21:56:58 +1000

    feat: adding Lumigo

--- plans/outputs.tf
+++ plans/outputs.tf
@@ -1,5 +1,5 @@
 output "feed_processor_darklist_arn" {
-    value = aws_lambda_function.feed_processor_darklist.arn
+  value = aws_lambda_function.feed_processor_darklist.arn
 }
 output "feed_processor_darklist_role" {
   value = aws_iam_role.feed_processor_darklist_role.name
